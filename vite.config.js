e3ce5609f7ce6b8a1946db82b6081428f50520b9
Maddy <madhavaraj1111@gmail.com>
2024-08-08 16:19:08 +0530

    fix: vite configure

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 
-
 export default defineConfig({
-  base: '/know-your-ip/'
+  base: "/know-your-ip/",
   //   build: {
   //     rollupOptions: {
   //       input: {
