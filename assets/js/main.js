9c851b35416508982595e23866ea14685faf96e3
Maddy <madhavaraj1111@gmail.com>
2024-08-08 15:59:19 +0530

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-// Write your javascript code here
+const ip=document.querySelector("#ipAddress");
+
+const xhr=new XMLHttpRequest();
+
+xhr.onreadystatechange=()=>{
+    if(xhr.readyState==4 && xhr.status==200){
+        ip.textContent=xhr.responseText;
+    }
+   
+    
+}
+
+xhr.open("GET",
+    "https://ipv4.icanhazip.com"
+)
+xhr.send();
+console.log(xhr);
